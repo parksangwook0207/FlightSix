c28148a7ee9834b3833f931d33e4f6c21986b6da
parksangwook0207 <124150788+parksangwook0207@users.noreply.github.com>
2023-03-03 19:57:57 +0900

    3.3
    작업중

--- Assets/Mine/0.Scirpt/Mine.cs
+++ Assets/Mine/0.Scirpt/Mine.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class Mine : MonoBehaviour
 {
     [SerializeField] private NewMine prefab;
     [SerializeField] private Transform parent;
+    [SerializeField] private GameObject clpan;
+    [SerializeField] private GameObject panel;
+
+    [SerializeField] private GridLayoutGroup grid;
 
     int[,] map;
 
-    int size = 5;
+    [SerializeField] private int size = 5;
+    [SerializeField] private int mineCnt = 1;
+
+    private List<List<NewMine>> mineList = new List<List<NewMine>>();
     // Start is called before the first frame update
     void Start()
     {
+        grid.constraintCount = size;
+
         map = new int[size, size];
         int[] tempMap = new int [size *size];
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < mineCnt; i++)
         {
             tempMap[i] = -1;
         }
@@ -51,25 +61,28 @@ public class Mine : MonoBehaviour
 
         for (int i = 0; i < size; i++)
         {
+            mineList.Add(new List<NewMine>());
             for (int j = 0; j < size; j++)
             {
                 NewMine m = Instantiate(prefab, parent);
-                m.Init(map[i, j]);
+                m.Init(map[i, j], i, j);
+                mineList[i].Add(m);
             }
-        }     
+        }
     }
 
-   void FindMine(int x, int y)
+    // ¿¹¿ÜÃ³¸®
+     void FindMine(int x, int y) // ²ÀÁþÁ¡ °Ë»ç
     {
-        for (int i = x-1; i <= x+1; i++)
+        for (int i = x - 1; i <= x + 1; i++)
         {
-            if (i < 0 ||i >= size)
+            if (i < 0 || i >= size)
             {
                 continue;
             }
-            for (int j = y-1; j <= y+1; j++)
+            for (int j = y - 1; j <= y + 1; j++)
             {
-                if (j < 0 || i >= size)
+                if (j < 0 || j >= size)
                 {
                     continue;
                 }
@@ -77,8 +90,51 @@ public class Mine : MonoBehaviour
                 {
                     continue;
                 }
-                    map[i, j]++;
+                map[i, j]++;
             }
+        }
+    }
+    public void AutoOpen(int x, int y)
+    {
+        for (int i = x - 1; i <= x + 1; i++)
+        {
+            if (i < 0 || i >= size)
+            {
+                continue;
+            }
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                if (j < 0 || j >= size)
+                {
+                    continue;
+                }
+                if (i == x && j == y)
+                {
+                    continue;
+                }
+                if (map[i, j] == 0)
+                {
+                    mineList[i][j].Open();
+                    clickCnt++;
+                }
+            }
+        }
+    }
+
+    public void ShowOverPancl() // ÆÇ³Ú 
+    {
+        panel.SetActive(true);
+    }
+
+    int clickCnt = 0;
+    public void Clpan() // ÆÇ³Ú 
+    {
+        clickCnt++;
+        int count = (size * size);
+
+        if (count == mineCnt)
+        {
+            clpan.SetActive(true);
         }
     }
 }
--- Assets/Mine/0.Scirpt/NewMine.cs
+++ Assets/Mine/0.Scirpt/NewMine.cs
@@ -9,23 +9,54 @@ public class NewMine : MonoBehaviour
     [SerializeField] private TMPro.TMP_Text numText;
     [SerializeField] private Image panel;
 
-    public void Init(int val)
+    int val, x, y;
+    public void Init(int val, int x, int y)
     {
-        if (val == -1)
+        this.val = val;
+        this.x = x;
+        this.y = y;
+
+        switch (val)
         {
-            mineImage.gameObject.SetActive(true);
-            numText.gameObject.SetActive(false);
-        }
-        else
-        {
-            mineImage.gameObject.SetActive(false);
-            numText.gameObject.SetActive(true);
-            numText.text = val.ToString();
+            case -1:
+                mineImage.gameObject.SetActive(true);
+                numText.gameObject.SetActive(false);
+                break;
+            case 0:
+                mineImage.gameObject.SetActive(false);
+                numText.gameObject.SetActive(true);
+                numText.text = string.Empty;
+                break;
+            default:
+                mineImage.gameObject.SetActive(false);
+                numText.gameObject.SetActive(true);
+                numText.text = val.ToString();
+                break;
+
+
         }
     }
 
     public void OnPanelOpen()
     {
+        GetComponent<Button>().interactable = false;
+        panel.gameObject.SetActive(false);
+        Mine cont = FindAnyObjectByType<Mine>();
+        if (val == 0)
+        {
+            FindAnyObjectByType<Mine>().AutoOpen(x, y);
+        }
+        else if (val == -1)
+        {
+            FindAnyObjectByType<Mine>().ShowOverPancl();
+        }
+
+        cont.Clpan();
+    }
+
+    public void Open()
+    {
         panel.gameObject.SetActive(false);
     }
 }
+
